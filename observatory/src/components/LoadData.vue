d286e5154bb761fe27a49f67a729dc6597fca257
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2022-06-20 16:49:19 +0100

    Spotted an async problem. Now resolved. Probably a better way to do this, but this works for now

--- observatory/src/components/LoadData.vue
+++ observatory/src/components/LoadData.vue
@@ -5,8 +5,8 @@ export default{
         return{
             manufactured_data:null,
             playground_data:null,
-            ai_lab_data_updated:null,
-            al_lab_data_first:null
+            ai_lab_2:null,
+            ai_lab_1:null
         };
     },
     created(){
@@ -18,20 +18,19 @@ export default{
             .then(response=>(this.playground_data=response));
         fetch('/src/assets/datasets/ai_lab_2.ttl')
             .then(response=>response.text())
-            .then(response=>(this.ai_lab_data_updated=response));
+            .then(response=>(this.ai_lab_2=response));
         fetch('/src/assets/datasets/ai_lab_1.ttl')
             .then(response=>response.text())
-            .then(response=>(this.al_lab_data_first=response));
+            .then(response=>(this.ai_lab_1=response));
     },
     methods:{
-        update_data(choice){
-
-            //first, empty the database
+        delete_data(choice){ //first, empty the database
             fetch('api/graph?DROP ALL',{
                 method:"DELETE"
             })
-            
-            //then re-fill it with the data we want to look at
+                .then(response=>this.update_data(choice))
+        },
+        update_data(choice){ //then re-fill it with the data we want to look at            
             switch(choice){ 
                 case "manufactured":
                     fetch('api/graph?',{
@@ -47,26 +46,28 @@ export default{
                         body: this.playground_data
                     });
                     break;
-                case "ai_lab":
+                case "ai_lab_2":
                     fetch('api/graph?',{
                         method:"POST",
                         headers:{"Content-Type":"application/x-turtle-RDR"},
-                        body:this.ai_lab_data_updated
+                        body:this.ai_lab_2
                     })
                     break;
-                case "ai_lab_initial_data":
+                case "ai_lab_1":
                     fetch('api/graph?',{
                         method:"POST",
                         headers:{"Content-Type":"application/x-turtle-RDR"},
-                        body:this.al_lab_data_first
+                        body:this.ai_lab_1
                     })
                     break;
                 default:
                     alert("If you have seen this message, please note down how and let the OHOS TNA team know - it should be unreachable from the UI!")
             }
+                
         }
     }
 }
+
 </script>
 
 <template>
@@ -74,23 +75,23 @@ export default{
 <fieldset>
 
     <div>
-        <input type="radio" id="manufactured" name="data_set" value="manufactured" @click="update_data('manufactured')">
+        <input type="radio" id="manufactured" name="data_set" value="manufactured" @click="delete_data('manufactured')">
         <label for="manufactured">Manufactured</label>
     </div>
 
     <div>
-        <input type="radio" id="playground" name="data_set" value="playground" @click="update_data('playground')">
+        <input type="radio" id="playground" name="data_set" value="playground" @click="delete_data('playground')">
         <label for="playground">Playground</label>
     </div>
 
     <div>
-        <input type="radio" id="ai_lab_initial_data" name="data_set" value="ai_lab_initial_data" @click="update_data('ai_lab_initial_data')">
-        <label for="ai_lab_initial_data">Ai Lab initial dataset</label>
+        <input type="radio" id="ai_lab_1" name="data_set" value="ai_lab_1" @click="delete_data('ai_lab_1')">
+        <label for="ai_lab_1">Ai Lab initial dataset</label>
     </div>
 
     <div>
-        <input type="radio" id="ai_lab" name="data_set" value="ai_lab" @click="update_data('ai_lab')">
-        <label for="ai_lab">Ai Lab second dataset</label>
+        <input type="radio" id="ai_lab_2" name="data_set" value="ai_lab_2" @click="delete_data('ai_lab_2')">
+        <label for="ai_lab_2">Ai Lab second dataset</label>
     </div>
 
 </fieldset>
