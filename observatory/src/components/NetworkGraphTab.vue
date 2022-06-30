9b3ed848f88f3eb6d3a2b5ad06e19717ef66bcfd
Harshad Gupta <harshad.gupta@nationalarchives.gov.uk>
2022-06-30 15:20:54 +0100

    OHOS 432 graph nodes correction

--- observatory/src/components/NetworkGraphTab.vue
+++ observatory/src/components/NetworkGraphTab.vue
@@ -17,6 +17,16 @@ export default{
             nodes: {},
             edges: {},
             layouts: {},
+            nodeLabel: {},
+            nodeLabelObje: {},
+            edgeLabel: {},
+            refArrayNode: null,
+            refNode: null,
+            refArrayNodeObje: null,
+            refNodeObje: null,
+            refArrayEdge: null,
+            refEdge: null,
+                
             configs: reactive(
               vNG.defineConfigs({
                 view: {
@@ -34,7 +44,13 @@ export default{
                   },
                 },
               })
-            )
+            ),
+            eventHandlers: {
+              "node:click": ({ node }) => {
+                console.log("Harshad");
+                alert(node);
+              },
+            }
         };
     },
     created(){
@@ -48,17 +64,114 @@ export default{
     methods:{
         draw_graph(fetched_data){
             var results = fetched_data.results.bindings;
+            var OHOSLink = "https://ohos.ac.uk/wp-content/uploads/2021/12/cropped-OHOSIcon_Large.png"
             for (let i = 0; i < results.length; i++){
                 let obj = results[i];
                 var sub = obj.s.value;
                 var pre = obj.p.value;
                 var obje = obj.o.value;
-                this.nodes[obje] = { name: obje };
-                this.edges[i] = { source: sub, target: obje, label: pre };
-                if(pre=="http://www.wikidata.org/prop/direct/P18")
-                    this.nodes[sub] = { name: sub, face: obje };
+                var imageCategory = ["P18","P109","P154","P41","P94","P948","P242","P1621","P2716","P3451","P4291","P8592","P2910"];
+                this.refArrayNode = sub.split("/");
+                this.refNode = this.refArrayNode[this.refArrayNode.length-1];
+                /*fetch(
+                      "https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&origin=*&format=json&&formatversion=2&ids="+this.refNode,
+                      {
+                        method: "GET"
+                      }
+                    )
+                      .then(response => response.json())
+                      .then(response => (this.nodeLabel=response))
+                      .then(response => {
+                        console.log(this.nodeLabel.entities[this.refNode].labels.en.value);
+                      })
+                      .catch(error => {
+                        console.log(error.message);
+                      });
+                */
+                this.refArrayNodeObje = obje.split("/");
+                this.refNodeObje = this.refArrayNodeObje[this.refArrayNodeObje.length-1];
+                /*fetch(
+                      "https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&origin=*&format=json&&formatversion=2&ids="+this.refNodeObje,
+                      {
+                        method: "GET"
+                      }
+                    )
+                      .then(response => response.json())
+                      .then(response => (this.nodeLabelObje=response))
+                      .then(response => {
+                        console.log(this.nodeLabelObje.entities[this.refNodeObje].labels.en.value);
+                      })
+                      .catch(error => {
+                        console.log(error.message);
+                      });
+                */
+                this.refArrayEdge = pre.split("/");
+                this.refEdge = this.refArrayEdge[this.refArrayEdge.length-1];
+                /*fetch(
+                      "https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&origin=*&format=json&&formatversion=2&ids="+this.refEdge,
+                      {
+                        method: "GET"
+                      }
+                    )
+                      .then(response => response.json())
+                      .then(response => (this.edgeLabel=response))
+                      .then(response => {
+                        console.log(this.edgeLabel.entities[this.refEdge].labels.en.value);
+                      })
+                      .catch(error => {
+                        console.log(error.message);
+                      });
+                console.log(this.refEdge);
+                console.log(this.edgeLabel.entities);
+                console.log(this.edgeLabel.entities[this.refEdge].labels.en.value);
+                */
+                var edgeLabelIsImage = imageCategory.includes(this.refEdge);//this.edgeLabel.entities[this.refEdge].labels.en.value=="image";
+                
+                switch(true){
+                    case(edgeLabelIsImage):
+                        if(!this.nodes[sub] || this.nodes[sub].face == OHOSLink)
+                            this.nodes[sub] = { name: this.refNode, face: obje };
+                        else
+                            if(this.nodes[sub])
+                            {
+                                this.nodes[obje] = { name: this.refNode, face: obje };
+                                this.edges[i] = { source: sub, target: obje, label: this.refEdge };
+                            }
+                        break;
+                    case(!edgeLabelIsImage):
+                        switch(true){
+                            case(!this.nodes[sub]):
+                                this.nodes[sub] = { name: this.refNode, face: OHOSLink };
+                                //if(!this.nodes[obje])
+                                    this.nodes[obje] = { name: this.refNode, face: OHOSLink };
+                                break;
+                            case(this.nodes[sub]):
+                                this.nodes[obje] = { name: this.refNode, face: OHOSLink };
+                                break;
+                        }
+                        console.log(this.nodes[obje]);
+                        console.log(obje);
+                        console.log(sub);
+                        console.log(this.refEdge);
+                        this.nodes[obje] = { name: this.refNode, face: OHOSLink };
+                        this.edges[i] = { source: sub, target: obje, label: this.refEdge };
+                        break;
+                }
+                /*if(edgeLabelImage && !this.nodes[sub])
+                    this.nodes[sub] = { name: this.nodeLabel.entities[this.refNode].labels.en.value, face: obje };
                 else
-                    this.nodes[sub] = { name: sub };
+                    {
+                        if(this.edgeLabel.entities[this.refEdge].labels.en.value=="image" && this.nodes[sub])
+                            this.nodes[sub] = { name: this.nodeLabel.entities[this.refNode].labels.en.value};
+                        if(!this.edgeLabel.entities[this.refEdge].labels.en.value=="image" && this.nodes[sub])
+                            this.nodes[sub] = { name: this.nodeLabel.entities[this.refNode].labels.en.value, face: "src\assets\ohos.png"};
+                        if(this.edgeLabel.entities[this.refEdge].labels.en.value=="image")
+                            this.nodes[obje] = { name: this.nodeLabel.entities[this.refNode].labels.en.value, face: obje};
+                        else              
+                            this.nodes[obje] = { name: this.nodeLabelObje.entities[this.refNodeObje].labels.en.value, face: "src\assets\ohos.png" };
+                        this.edges[i] = { source: sub, target: obje, label: this.edgeLabel.entities[this.refEdge].labels.en.value };
+                    }
+                */
             }
         }
     }
@@ -74,6 +187,7 @@ export default{
     :layouts="layouts"
     :configs="configs"
     :layers="layers"
+    :event-handlers="eventHandlers"
   >
     <defs>
       <clipPath id="faceCircle" clipPathUnits="objectBoundingBox">
