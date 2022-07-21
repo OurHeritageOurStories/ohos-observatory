6a3ee849b1a826b51a5a17c8e2827dfad0c56c15
wnybomTNA <Waltteri.Nybom@nationalarchives.gov.uk>
2022-07-21 13:46:23 +0100

    Hazel's text added

--- observatory/src/components/AboutTab.vue
+++ observatory/src/components/AboutTab.vue
@@ -2,7 +2,11 @@
 
 <div id="about_tab">
     <h3 id="about_tab_heading">Welcome to OHOS Observatory</h3>
-    <p id="about_tab_content">Please select a tab</p>
+    <p id="about_tab_content">Welcome to the Observatory - revealing linked community-generated digital content (CGDC). <br><br>
+    The Observatory enables access to previously unfindable and unlinkable CGDC for research and discovery, through visualisations and tools.<br><br>
+    Use the 'Explore' tab to discover the linked CGDC. Here you'll find a network graph view. Zoom and move the nodes to explore the data and relationships.<br><br>
+    Share your knowledge in the Observatory by adding new links and entities in the 'Add' tab, or explore another linked data dataset via a URL or file upload in the 'Change Dataset' tab.<br><br>
+    The Observatory has been created as part of the <a href="https://ohos.ac.uk/"  target="_blank">Our Heritage, Our Stories</a> project, one strand of the <a href="https://www.nationalcollection.org.uk/"  target="_blank">Towards a National Collection</a> programme.</p>
 </div>
 
 </template>
@@ -20,5 +24,6 @@
     grid-row-end: 4;
     padding: 10px;
     padding-top: 25px;
+    max-width: 700px;
 }
 </style>
