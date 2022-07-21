b47c6b4ced24c56ac443a3047ba4f5c89aac2384
Harshad Gupta <harshad.gupta@nationalarchives.gov.uk>
2022-07-21 17:41:54 +0100

    retained prefix in data, removed it in frontend

--- observatory/src/assets/datasets/ai_lab_extracter.py
+++ observatory/src/assets/datasets/ai_lab_extracter.py
@@ -3,5 +3,5 @@ triplets = []
 f = open("ai_lab_2_extracted.ttl", "w")
 for i in lines:
 	if "tanc:source" in i:
-		line = i.replace("<<<http://dbpedia.org/resource/", "<:").replace("tanc", "<").replace(" <http://dbpedia.org/resource/", "> <:").replace(">>>", "> .\n>>").split(">>")[0]
+		line = i.replace("<<<", "<:").replace("tanc", "<").replace(" <http://dbpedia.org/resource/", "> <:http://dbpedia.org/resource/").replace(">>>", "> .\n>>").split(">>")[0]
 		f.write(line)
