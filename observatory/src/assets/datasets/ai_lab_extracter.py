7cbaea88b17defaf8b15c37efd7177d896da6d21
Harshad Gupta <harshad.gupta@nationalarchives.gov.uk>
2022-07-22 11:41:09 +0100

    added othere data!

--- observatory/src/assets/datasets/ai_lab_extracter.py
+++ observatory/src/assets/datasets/ai_lab_extracter.py
@@ -1,7 +1,20 @@
 lines = open(r'ai_lab_2.ttl').read().split('\n')
 triplets = []
 f = open("ai_lab_2_extracted.ttl", "w")
-for i in lines:
+for j, i in enumerate(lines):
 	if "tanc:source" in i:
-		line = i.replace("<<<", "<:").replace("tanc", "<").replace(" <http://dbpedia.org/resource/", "> <:http://dbpedia.org/resource/").replace(">>>", "> .\n>>").split(">>")[0]
+		line = i.replace("<<<", "<").replace("tanc", "<").replace(" <http://dbpedia.org/resource/", "> <http://dbpedia.org/resource/").replace(">>>", "> .\n>>").split(">>")[0]
 		f.write(line)
+		#print(line)
+	elif "tanc:similarity" in i:
+		subject = i.split("tanc:")[1][9:-3]
+		while "tanc:start" not in lines[j+1]:
+			#print(subject, lines[j+1].split(":")[1].split(" ")[1], lines[j+1].split(":")[2].split(" ")[0])
+			if lines[j+1].split(":")[1].split(" ")[1] == "Wikidata":
+				obj = "<http://www.wikidata.org/entity/" + lines[j+1].split(":")[2].split(" ")[0] + "> ."
+			else:
+				obj = "<http://dbpedia.org/resource/" + lines[j+1].split(":")[2].split(" ")[0] + "> ."
+			line = subject + " <:> " + obj
+			j=j+1
+			#print(line)
+			f.write(line)
