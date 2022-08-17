a89049fee48639221df9b638168938a8b6c951aa
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2022-08-17 11:38:14 +0100

    Environmental variables rather than hardcoding

--- observatory/vite.config.js
+++ observatory/vite.config.js
@@ -1,27 +1,57 @@
 import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server:{
-    host: true,
-    proxy:{
-      '/foo':'http://ohos_observatory_kong:8000',
-      '/api':{
-        target:'http://ohos_observatory_kong:8000',
-        changeOrigin:true,
-        secure:false,
-        ws:true,
-        rewrite:(path)=>path.replace(/^\/api/, '')
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins:[vue()],
+    resolve:{
+      alias:{
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server:{
+      host:true,
+      proxy:{
+        '/foo':process.env.VITE_API_ENDPOINT,
+        '/api':{
+          target:process.env.VITE_API_ENDPOINT,
+          changeOrigin:true,
+          secure:false,
+          ws:true,
+          rewrite:(path)=>path.replace(/^\/api/, '')
+        }
       }
     }
-  }
-})
+  })
+}
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+//  plugins: [vue()],
+//  resolve: {
+//    alias: {
+//      '@': fileURLToPath(new URL('./src', import.meta.url))
+//    }
+//  },
+//  envDir:'./',
+//  server:{
+//    host: true,
+//    proxy:{
+//      //'/foo':'http://ohos_observatory_kong:8000',
+//      //'/foo':import.meta.env.VITE_API_ENDPOINT,
+//      '/foo':import.meta.env.VITE_API_ENDPOINT,
+//      '/api':{
+//        //target:'http://ohos_observatory_kong:8000',
+//        //target: import.meta.env.VITE_API_ENDPOINT,
+//        target:import.meta.env.VITE_API_ENDPOINT,
+//        changeOrigin:true,
+//        secure:false,
+//        ws:true,
+//        rewrite:(path)=>path.replace(/^\/api/, '')
+//      }
+//    }
+//  }
+//})
