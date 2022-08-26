77b138a8fc39aef88ac4dbbe358af49eafbd83fb
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2022-08-26 13:21:31 +0100

    removal of unintentially included folders

--- node_modules/tify/tests/e2e/specs/fulltext.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Fulltext', () => {
-	it('Display fulltext', () => {
-		cy.visit('/?manifest=http://localhost:8081/manifest/wellcome-b18035723.json&tify={"pages":[15]}');
-		cy.get('.tify').then(() => {
-			cy.contains('Fulltext').click();
-			cy.contains('Alles höhere Leben - ob Tier oder').should('be.visible');
-		});
-	});
-});
